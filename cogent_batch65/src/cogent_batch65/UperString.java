96d2b4d961457bd0c4976d57498111de0025ea98
khoahuynh88 <khoahuynh1088@gmail.com>
2023-03-24 07:11:18 -0500

    firday test

--- cogent_batch65/src/cogent_batch65/UperString.java
+++ cogent_batch65/src/cogent_batch65/UperString.java
@@ -1,15 +1,23 @@
 package cogent_batch65;
 
 public class UperString {
-
-	 public static boolean isPalindrome(String original) {
-
-	        String reversed = new StringBuilder(original).reverse().toString();
-	        return original.equals(reversed);
-	    }
-
+	   
 	public static void main(String[] args) {
-		
+		if (args.length >0) 
+		{
+			int length = args[0].length();
+			
+			String s = args[0];
+			 System.out.println("String length" + length);
+			 System.out.println("Upercase"+s.toUpperCase());
+			 
+			 String reverse = new String();
+			 
+			 for (int i= s.length() -1 ; i>=0; i--)
+				 reverse +=s.charAt(i);
+			 if (reverse.equalsIgnoreCase(s)   ) System.out.println("true");
+			 else System.out.println("false");
+		}
 
 	}
 
--- testproject/src/testproject/Test.java
+++ testproject/src/testproject/Test.java
@@ -4,7 +4,18 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int number =3;
+		
+		if (number >=0) 
+		{
+			if (number ==0) 
+			{
+				System.out.println("first string");
+			}
+			else
+				System.out.println("second string");
+				System.out.println("third string");
+		}
 	}
 
 }
